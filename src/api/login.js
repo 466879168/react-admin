a2a5aebe24e8fdd207c1600a7c8d603765b37479
466879168 <466879168@qq.com>
2020-08-17 22:05:20 +0800

    获取验证码细节

--- src/api/login.js
+++ src/api/login.js
@@ -1,10 +1,25 @@
 import service from '../utils/request'
 
+/**
+ * 登录接口
+ * @param data
+ */
 export function login(data){
 	service.request({
 		url:"/login/",
 		method:"post",
 		data:data,//请求参数
 		//params:data//请求类型为get时
+	})
+}
+
+/**
+ * 获取验证码
+ */
+export function GetCode(data){
+	return service.request({
+		url:"/getSms/",
+		method:"post",
+		data
 	})
 }
--- src/view/login/Login.jsx
+++ src/view/login/Login.jsx
@@ -2,15 +2,22 @@ import React,{Component,Fragment} from 'react'
 import { Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { Row, Col } from 'antd';
-import {login} from "../../api/login"
+import { message, Space } from 'antd';
+import {login,GetCode} from "../../api/login"
 import "./index.scss";
 
 export default class Login extends Component {
   constructor(){
     super()
-    this.state={}
+    this.state={
+      username:"",
+      codeBtnDisabled:true,
+      codeBtnLoading:false,
+      btnText:"获取验证码"
+    }
   }
   onFinish = values => {
+    console.log(values)
     login().then(response=>{
 
     }).catch(error=>{
@@ -18,9 +25,39 @@ export default class Login extends Component {
     })
     console.log('Received values of form: ', values);
   };
+  getCode=()=>{
+    if (!this.state.username){
+      message.error('请输入用户名！');
+      return
+    }
+    const params={
+      username:this.state.username,
+      module:"login"
+    }
+    GetCode().then(response=>{
+      console.log(response)
+    }).catch(error=>{
+      console.log(error)
+      this.setState({
+        codeBtnLoading:false,
+        btnText:"重新获取"
+      })
+    })
+  }
   goToRegister=() => {
     this.props.loginType("register")
     console.log(11111)
+  }
+
+  /**
+   *
+   */
+  inputChange(e){
+    let value=e.target.value
+    this.setState({
+      username:value
+    })
+    console.log(e)
   }
   render(){
     return (
@@ -46,7 +83,7 @@ export default class Login extends Component {
                   },
                 ]}
             >
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+              <Input value={this.state.username} onChange={this.inputChange} prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
             </Form.Item>
             <Form.Item
                 name="password"
@@ -75,7 +112,7 @@ export default class Login extends Component {
                   />
                 </Col>
                 <Col className="gutter-row" span={9}>
-                  <Button type="danger"  className="login-form-button">获取验证码</Button>
+                  <Button disabled={this.state.codeBtnDisabled} loading={codeBtnLoading} type="danger"  className="login-form-button">{this.state.btnText}</Button>
                 </Col>
               </Row>
             </Form.Item>
